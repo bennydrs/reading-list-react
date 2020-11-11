d350a1386e6106cc0bfadd63ab5949fc6fb33bcb
bennydrspc <bennydrm16@gmail.com>
2020-11-11 08:46:13 +0700

    adding form

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Navbar from './components/Navbar';
 import BookContextProvider from './contexts/BookContext';
 import BookList from './components/BookList';
+import BookForm from './components/BookForm';
 
 function App() {
   return (
     <div className="App">
       <BookContextProvider>
         <Navbar />
+        <BookForm />
         <BookList/>
       </BookContextProvider>
     </div>
